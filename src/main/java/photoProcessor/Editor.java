0f105d03be713e90abac612315f75d00772cf47a
Alexander <berezkin05@gmail.com>
2018-04-06 10:00:52 +0300

    Refactor code, add rotation checking

--- src/main/java/photoProcessor/Editor.java
+++ src/main/java/photoProcessor/Editor.java
@@ -1,5 +1,11 @@
 package photoProcessor;
 
+import com.drew.imaging.jpeg.JpegMetadataReader;
+import com.drew.imaging.jpeg.JpegProcessingException;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.MetadataException;
+import com.drew.metadata.exif.ExifIFD0Directory;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
@@ -10,45 +16,58 @@ import java.io.IOException;
 
 public class Editor {
 
-    private static int wh = 960;
-    private static BufferedImage im1, im3;
-    private static File f, f1, f2;
+    private static int pattern;
+    private static BufferedImage imgIn, imgOut;
+    private static File f;
     private static Graphics2D graph;
     private static AffineTransform tx = new AffineTransform();
-    private String pathCanvas;
     private String pathObject;
-    private String pathCreator;
 
-    Editor(String pathCanvas, String pathObject, String pathCreator) {
-        this.pathCanvas = pathCanvas;
+    Editor(String pathObject) {
         this.pathObject = pathObject;
-        this.pathCreator = pathCreator;
     }
 
     public void edit() {
-        f = new File(pathCanvas);
-        f1 = new File(pathCreator);
-        f2 = new File(pathObject);
+        f = new File(pathObject);
 
         try {
 
 //            reading files
-            im1 = ImageIO.read(f);
-            im3 = ImageIO.read(f2);
+            imgIn = ImageIO.read(f);
             System.out.println("Reading complete.");
 
+//            set patter
+            pattern = imgIn.getWidth();
+
 //            drawing
-            graph = im1.createGraphics();
-            graph.drawImage(im3, null, findPoint(wh, im3.getWidth()), findPoint(wh, im3.getHeight()));
-            im1 = rotateImage(im1, tx);
+            imgOut = new BufferedImage(pattern, pattern, BufferedImage.TYPE_3BYTE_BGR); //create background
+            graph = imgOut.createGraphics();
+            graph.setPaint(new Color(255, 255, 255));//background color (here - white)
+            graph.fillRect(0, 0, pattern, pattern);//fill background with color^^
+            graph.drawImage(imgIn, null, findPoint(pattern, imgIn.getWidth()), findPoint(pattern, imgIn.getHeight()));
+            imgOut = toRotate(imgOut, f);// check for rotation
 
 //            Saving file
-            ImageIO.write(im1, "jpg", f1);
+            ImageIO.write(imgOut, "jpg", new File("D:\\app\\Tests\\testOut.jpg"));
             System.out.println("Writing complete.");
         } catch (IOException e) {
             System.out.println("Error: " + e);
+        } catch (JpegProcessingException e) {
+            e.printStackTrace();
+        } catch (MetadataException e) {
+            e.printStackTrace();
         }
 
+    }
+
+//      Check rotating need
+    private static BufferedImage toRotate(BufferedImage img, File file) throws IOException, JpegProcessingException, MetadataException {
+        Metadata metadata = JpegMetadataReader.readMetadata(file);
+        int or = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class).getInt(ExifIFD0Directory.TAG_ORIENTATION);
+        if (or != 8) {
+            return img;
+        }
+        return rotateImage(img, tx);
     }
 
     //    Rotating image
@@ -67,20 +86,12 @@ public class Editor {
 
 //    getters and setters
 
-    public static BufferedImage getIm1() {
-        return im1;
+    public static BufferedImage getImgIn() {
+        return imgIn;
     }
 
-    public static void setIm1(BufferedImage im1) {
-        Editor.im1 = im1;
-    }
-
-    public static BufferedImage getIm3() {
-        return im3;
-    }
-
-    public static void setIm3(BufferedImage im3) {
-        Editor.im3 = im3;
+    public static void setImgIn(BufferedImage imgIn) {
+        Editor.imgIn = imgIn;
     }
 
     public static File getF() {
@@ -89,22 +100,6 @@ public class Editor {
 
     public static void setF(File f) {
         Editor.f = f;
-    }
-
-    public static File getF1() {
-        return f1;
-    }
-
-    public static void setF1(File f1) {
-        Editor.f1 = f1;
-    }
-
-    public static File getF2() {
-        return f2;
-    }
-
-    public static void setF2(File f2) {
-        Editor.f2 = f2;
     }
 
     public static Graphics2D getGraph() {
@@ -123,27 +118,11 @@ public class Editor {
         Editor.tx = tx;
     }
 
-    public String getPathCanvas() {
-        return pathCanvas;
-    }
-
-    public void setPathCanvas(String pathCanvas) {
-        this.pathCanvas = pathCanvas;
-    }
-
     public String getPathObject() {
         return pathObject;
     }
 
     public void setPathObject(String pathObject) {
         this.pathObject = pathObject;
-    }
-
-    public String getPathCreator() {
-        return pathCreator;
-    }
-
-    public void setPathCreator(String pathCreator) {
-        this.pathCreator = pathCreator;
     }
 }
--- src/main/java/photoProcessor/PhotoProcessor.java
+++ src/main/java/photoProcessor/PhotoProcessor.java
@@ -3,11 +3,10 @@ package photoProcessor;
 public class PhotoProcessor {
 
     public static void main(String[] args) {
-        String path1 = "D:\\app\\Tests\\Background.jpg";
-        String path2 = "D:\\app\\Tests\\test.jpg";
-        String path3 = "D:\\app\\Tests\\output.jpg";
 
-        Editor editor = new Editor(path1, path2, path3);
+        String path2 = "D:\\app\\Tests\\test.jpg";
+
+        Editor editor = new Editor(path2);
 
         editor.edit();
     }
